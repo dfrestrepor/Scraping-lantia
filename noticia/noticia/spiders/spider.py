f6d1841a8f59fa3498d019fea98a0d78b9039121
JOHANA <johagomez.4@gmail.com>
2020-01-16 10:40:25 -0500

    intento de creación de csv

--- noticia/noticia/spiders/spider.py
+++ noticia/noticia/spiders/spider.py
@@ -11,39 +11,29 @@ from newspaper import Article
 import csv
 
 
-rutas={'Dominio':['www.sciencenews.org', 'https://www.screenskills.com/'],#, 'http://designpackagingnews.com'],
-       'camino':['//h3/a', '//article/a'],#, '//h2/a'],
-       'inicial_page':['https://www.sciencenews.org/topics', 'https://www.screenskills.com/']}#, 'http://designpackagingnews.com/en/']}
+class web_spider(CrawlSpider):
+	name = 'noticias'
+	item_count = 0
+	allowed_domain = ['www.sciencenews.org', 'https://www.screenskills.com/', 'http://designpackagingnews.com']
+	start_urls = ['https://www.sciencenews.org/topics', 'https://www.screenskills.com/', 'http://designpackagingnews.com/en/']
 
-lista = []
-for x in range(2):
-	class web_spider(CrawlSpider):
-		name = 'noticias'
-		item_count = 0
-		allowed_domain = [str(rutas['Dominio'][x])]
-		start_urls = [str(rutas['inicial_page'][x])]
 
-		def parse(self, response):
-			noticia = response.xpath(str(rutas['camino'][x]))
-			#print(noticia)
-			for value in noticia:
-				url = value.xpath('@href').extract()
-				article = Article(url[0])
-				article.download()
-				article.parse()
-				ws_item = NoticiaItem()
-				lista.append(article.title)
-				#lista.append( article.text)
-				lista.append('\n\n')
-				#print(lista)
-				#ws_item['link'] = article.url
-				#ws_item['pubdate'] = article.publish_date
-				yield ws_item
+	def parse(self, response):
+		noticia1 = response.xpath('//h3/a')
+		noticia2 = response.xpath('//article/a')
+		noticia3 = response.xpath('//h2/a')
+		noticia= noticia1+noticia2+noticia3
+		print(noticia)
+		for value in noticia:
+			url = value.xpath('@href').extract()
+			article = Article(url[0])
+			article.download()
+			article.parse()
+			ws_item = NoticiaItem()
+			ws_item['link'] = article.url
+			ws_item['pubdate'] = article.publish_date
+			yield ws_item
 
-			print(len(lista))
-with open("output.csv", "w") as f:
-	w = csv.writer(f)
-	w.writerow(lista)
 
 
 
